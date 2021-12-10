81f3be4236e80fe5247cba1410f648e0c1567e96
Crowdbotics <team@crowdbotics.com>
2021-12-10 18:11:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "araliya_messanger_32341",
+  url: "https://araliya_messanger_32341.botics.co",
+  api: "https://araliya_messanger_32341.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
